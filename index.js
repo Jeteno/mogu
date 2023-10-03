ec8bc5cd969e75334929ab64cd973422baafbe80
Maxim Sytov <107058656+Jeteno@users.noreply.github.com>
2023-10-03 10:19:17 +0300

    push

---
+++ index.js
@@ -0,0 +1,10 @@
+const iconMenu = document.querySelector('.menu__icon');
+ 
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener('click', function (e) {
+        iconMenu.classList.toggle('active__menu');
+        menuBody.classList.toggle('active__menu');
+    })
+}
+ 
